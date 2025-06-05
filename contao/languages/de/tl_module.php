59420223d06c7a3d80112fe599b25e5345aad406
janborg <jan+github@luenborg.eu>
2025-06-05 14:59:58 +0200

    add german translations in tl_module

--- contao/languages/de/tl_module.php
+++ contao/languages/de/tl_module.php
@@ -16,4 +16,10 @@
 $GLOBALS['TL_LANG']['tl_module']['cfg_instagramAppId'] = ['Instagram App ID', 'Bitte geben Sie die Instagram App ID ein.'];
 $GLOBALS['TL_LANG']['tl_module']['cfg_instagramAppSecret'] = ['Instagram App Secret', 'Bitte geben Sie das Instagram App Secret ein.'];
 $GLOBALS['TL_LANG']['tl_module']['cfg_instagramAccessToken'] = ['Instagram Access Token', 'Dies ist ein automatich erzeugter Wert, welcher beim Abschicken des Formulars generiert wird.'];
+$GLOBALS['TL_LANG']['tl_module']['cfg_instagramAccessTokenTstamp'] = ['Letztes Update des Instagram Access Token'];
 $GLOBALS['TL_LANG']['tl_module']['cfg_instagramRequestToken'] = ['Access Token anfordern', 'Aktivieren Sie diese Option um einen neuen Instagram Access Token anzufordern.'];
+$GLOBALS['TL_LANG']['tl_module']['cfg_skipSslVerification'] = ['SSL-Verifikation überspringen', 'Die SSL-Verifikation während API-Anfragen überspringen (nicht empfohlen).'];
+$GLOBALS['TL_LANG']['tl_module']['cfg_instagramStoreFiles'] = ['Instagram Dateien speichern', 'Speichere die Instagram-Dateien lokal.'];
+$GLOBALS['TL_LANG']['tl_module']['cfg_instagramStoreFolder'] = ['Ordner für gespeicherte Instagram-Dateien', 'Wählen Sie den Ordner aus, in dem die Instagram-Dateien gespeichert werden sollen.'];
+$GLOBALS['TL_LANG']['tl_module']['cfg_instagramMediaTypes'] = ['Instagram-Mediatypen', 'Wählen Sie hier die Instagram-Mediatypen aus, die angezeigt werden sollen.'];
+$GLOBALS['TL_LANG']['tl_module']['cfg_instagramFetchComments'] = ['Kommentare abrufen', 'Aktivieren Sie diese Option, um die Kommentar abzurufen.'];
